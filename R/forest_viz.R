040fc12dd80470381953fb75b3ee83da93455936
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-04 17:36:04 -0600

    Now is Package

---
+++ R/forest_viz.R
@@ -0,0 +1,74 @@
+#' Visualization of the Variables of Importance
+#' 
+#' Visualizes--using ggplot2--the relationship of n important variables 
+#' in a random forest with the outcome with a loess curve (if continuous).
+#' 
+#' @param obj a randomForest object
+#' @param n the number of important variables
+#' @param imp_measure the variable importance measure
+#' 
+#' @import ggplot2
+#' @import dplyr
+#' @import tidyr
+#' 
+#' @export
+forest_viz <- function(obj, n=10, imp_measure='default'){
+  
+  ## Data
+  imps = obtain_important(obj, n, imp_measure)
+  
+  ## Reformat data
+  .data_long = gather(imps, "var", "value", 1:n)
+
+  ## Graphic
+  ggplot(.data_long, aes(x=value, y=outcome, group=var)) +
+    geom_count(aes(color = var)) +
+    facet_wrap(~var, scales = "free") +
+    geom_smooth(color = "coral4") +
+    theme_anteo() +
+    guides(color = "none") +
+    labs(y = "Outcome",
+         x = "")
+}
+
+
+
+#' Obtain Important Variable Information
+#' 
+#' Internal tool for forest_viz and forest_tab1 functions. Can be used to get data
+#' from a Random Forest object.
+#' 
+#' @param obj a randomForest object
+#' @param n the number of important variables
+#' @param imp_measure the variable importance measure
+#' 
+#' @import ggplot2
+#' 
+#' @export
+obtain_important = function(obj, n=10, imp_measure='mse'){
+  .data = eval(obj$call$data)
+  .y = obj$y
+  
+  .d_imp = data.frame(obj$importance)
+  
+  if(obj$type == "regression"){
+    if (imp_measure=="mse"){
+      .imp = .d_imp[order(.d_imp[,1], decreasing = TRUE),]
+    } else {
+      .imp = .d_imp[order(.d_imp[,2], decreasing = TRUE),]
+    }
+  } else if (obj$type == "classification"){
+    .imp = d_imp[order(d_imp[,1], decreasing = TRUE),]
+  }
+  
+  nams = row.names(.imp)[1:n]
+  d = .data[nams]
+  d$outcome = .y
+  
+  return(d)
+}
+
+
+
+
+
--- R/theme_anteo.R
+++ R/theme_anteo.R
@@ -10,18 +10,20 @@
 #' 
 #' @export
 theme_anteo <- function(base_size = 12, base_family = "Helvetica"){
-  theme_bw(base_size = base_size, base_family = base_family) %+replace%
-    theme(legend.key=element_rect(color="grey99"),
-          panel.border=element_rect(color="grey99", size=.1),
-          axis.ticks.y=element_line(color="lightgrey"),
-          axis.ticks.x=element_line(color="lightgrey"),
-          strip.background=element_rect(fill="grey99", color="grey99"),
-          strip.text=element_text(face="bold"),
-          axis.line.x=element_line(color="grey70"),
-          axis.line.y=element_line(color="grey70"),
-          panel.spacing = unit(0, "lines"),
-          panel.background=element_rect(fill="grey99"),
-          plot.background=element_rect(fill="grey99"))
+  theme_bw(base_size = base_size, base_family = base_family) +
+  theme(legend.key=element_rect(color="grey99"),
+        legend.box.background=element_rect(color="grey99"),
+        legend.background=element_rect(color="grey99"),
+        panel.border=element_rect(color="grey99", size=.1),
+        axis.ticks.y=element_line(color="lightgrey"),
+        axis.ticks.x=element_line(color="lightgrey"),
+        strip.background=element_rect(fill="grey99", color="grey99"),
+        strip.text=element_text(face="bold"),
+        axis.line.x=element_line(color="grey70"),
+        axis.line.y=element_line(color="grey70"),
+        panel.spacing = unit(0, "lines"),
+        panel.background=element_rect(fill="grey99"),
+        plot.background=element_rect(fill="grey99"))
 }
 
 
@@ -39,17 +41,17 @@ theme_anteo <- function(base_size = 12, base_family = "Helvetica"){
 #' 
 #' @export
 theme_anteo_wh <- function(base_size = 12, base_family = "Helvetica"){
-  theme_anteo(base_size = base_size, base_family = base_family) %+replace%
-    theme(legend.key=element_rect(color="white"),
-          panel.border=element_blank(),
-          axis.ticks.y=element_line(color="grey50"),
-          axis.ticks.x=element_line(color="grey50"),
-          strip.background=element_rect(fill="white", color="white"),
-          strip.text=element_text(face="bold"),
-          axis.line.x=element_line(color="grey70"),
-          axis.line.y=element_line(color="grey70"),
-          panel.spacing = unit(0, "lines"),
-          panel.background=element_rect(fill="grey99"),
-          plot.background=element_rect(fill="white"))
+  theme_anteo(base_size = base_size, base_family = base_family) +
+  theme(legend.key=element_rect(color="white"),
+        panel.border=element_blank(),
+        axis.ticks.y=element_line(color="grey50"),
+        axis.ticks.x=element_line(color="grey50"),
+        strip.background=element_rect(fill="white", color="white"),
+        strip.text=element_text(face="bold"),
+        axis.line.x=element_line(color="grey70"),
+        axis.line.y=element_line(color="grey70"),
+        panel.spacing = unit(0, "lines"),
+        panel.background=element_rect(fill="grey99"),
+        plot.background=element_rect(fill="white"))
 }
 
