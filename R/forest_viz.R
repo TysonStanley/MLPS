ae7d4a6c65f270a971db6f9225608941db92e3ce
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-04 23:57:53 -0600

    outcome as vector in obtain_important

--- R/forest_viz.R
+++ R/forest_viz.R
@@ -71,7 +71,7 @@ obtain_important = function(obj, n=10, imp_measure='mse'){
   ## CForest Object
   else if (any(grepl("RandomForest", class(obj)))){
     .data = MEapply(obj@data, FUN=as.data.frame)
-    .y    = MEapply(obj@responses, FUN=as.vector)
+    .y    = MEapply(obj@responses, FUN=function(x) as.numeric(as.character(x)))
     
     .d_imp = varimp(obj)
     .imp   = sort(.d_imp, decreasing = TRUE)
