7d60a9d4bb0c696fc64bfb6684263895f4b6cf1e
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-05 00:16:10 -0600

    One more try...

--- R/forest_viz.R
+++ R/forest_viz.R
@@ -71,7 +71,7 @@ obtain_important = function(obj, n=10, imp_measure='mse'){
   
   ## CForest Object
   else if (any(grepl("RandomForest", class(obj)))){
-    .data = MEapply(obj@data, FUN=as.data.frame)
+    .data = MEapply(obj@data, FUN=data.frame)
     .y    = obj@responses
     
     .d_imp = varimp(obj)
