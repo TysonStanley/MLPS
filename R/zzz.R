83c3a2e3d82afb7d6fde6f8e11e255a2a977c891
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-07 15:19:44 -0700

    Extra message in start up

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onAttach <- function(libname = find.package("anteo"), pkgname = "anteo") {
-  packageStartupMessage("anteo 0.0.1: learn more at tysonstanley.github.io")
+  packageStartupMessage("anteo 0.0.1: learn more at tysonstanley.github.io \n
+                        ** ggplot2 needs to be updated to the current version as of Nov 8, 2016.")
 }
