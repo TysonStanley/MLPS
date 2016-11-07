782fe345ab918dc083b5dcd79dc41065b2fe4bc0
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-06 19:51:58 -0700

    full documentation and start up message

---
+++ R/anteo.R
@@ -0,0 +1,40 @@
+#' anteo
+#' 
+#' The anteo package offers tools that work with random forest objects (from the 
+#' \code{randomForest} package and conditional inference forest objects (from the 
+#' \code{party} package. 
+#' \itemize{
+#'   \item \code{forest_viz} a simple visualization tool for looking at the relationships
+#'          of the n most imporant variables and the outcome (for \code{randomForest})
+#'   \item \code{cforest_viz} a simple visualization tool for looking at the relationships
+#'          of the n most imporant variables and the outcome (for \code{cforest})
+#' }
+#' 
+#' There are also two simple \code{ggplot2} themes:
+#' \itemize{
+#'   \item \code{theme_anteo} 
+#'   \item \code{theme_anteo_wh} 
+#' }
+#' 
+#' Overall, the package helps with the exploration of large data sets.
+#' 
+#' Note: \textbf{anteo} means to prevent or go before in latin.
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' library(anteo)
+#' 
+#' ## Random Forest
+#' fit = randomForest(outcome ~ ., data=data)
+#' forest_viz(fit)
+#' 
+#' ## Conditional Inference Forest
+#' fit2 = cforest(outcome ~ ., data=data)
+#' cforest_viz(fit2)
+#' 
+#' }
+#' 
+#' @docType package
+#' @name anteo
+NULL
---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname = find.package("anteo"), pkgname = "anteo") {
+  packageStartupMessage("anteo 0.0.1: learn more at tysonstanley.github.io")
+}
