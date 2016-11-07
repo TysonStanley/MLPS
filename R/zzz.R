80a3d03d37daf42fdee146f943aafbf4d40ebab1
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-07 15:20:49 -0700

    fixed typo

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname = find.package("anteo"), pkgname = "anteo") {
-  packageStartupMessage("anteo 0.0.1: learn more at tysonstanley.github.io \n
-                        ** ggplot2 needs to be updated to the current version as of Nov 8, 2016.")
+  packageStartupMessage(paste("anteo 0.0.1: learn more at tysonstanley.github.io \n",
+                        "** ggplot2 needs to be updated to the current version as of Nov 8, 2016."))
 }
